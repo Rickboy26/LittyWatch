cc79f5a7091e62ac47762fd6c608723bf45e4086
Rickboy26 <rickdh123@gmail.com>
2026-08-07 23:57:52 +0200

    v4.6.12

--- app/Controllers/MaintenanceController.php
+++ app/Controllers/MaintenanceController.php
@@ -101,9 +101,9 @@ final class MaintenanceController
             'structured_offers_created' => $structuredCreated,
             'lifecycle' => $lifecycleResult,
             'market_quality' => $marketQualityResult,
-            'parser_fix' => 'Phase 3I market outlier & review detection actief.',
+            'parser_fix' => 'Phase 3L.3 price semantics + market quality actief.',
             'items_source' => 'structured_offers',
-            'parser_release' => 'V5.2 Phase 3I',
+            'parser_release' => 'V5.2 Phase 3L.3',
             'timestamps_repaired' => $timestampsRepaired,
         ], '/items');
     }
